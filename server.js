1d124cfcc42439c3a594dca5322b4ead7b39e93b
Holt Moore <holt.w.moore@gmail.com>
2023-07-31 20:00:46 -0500

    show route and index route

--- server.js
+++ server.js
@@ -1,21 +1,49 @@
 const express = require('express');
 const app = express();
-
+const port = 4000;
 
 const pokemonData = require('./models/pokemon');
 
 app.set('view engine', 'ejs');
 
-
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/pokemon', (req, res) => {
-  res.json(pokemonData);
+  res.render('index', { pokemonData });
+});
+
+app.get('/pokemon/:id', (req, res) => {
+  const pokemonId = req.params.id;
+  const selectedPokemon = pokemonData.find((pokemon) => pokemon.id === pokemonId);
+  if (selectedPokemon) {
+    res.render('show', { pokemon: selectedPokemon });
+  } else {
+    res.status(404).send('Pokémon not found');
+  }
+});
+
+app.get('/pokemon/new', (req, res) => {
+  res.render('new');
 });
 
 
+app.post('/pokemon', (req, res) => {
+  const newPokemon = {
+    id: generateNewId(),
+    name: req.body.name,
+    img: req.body.img,
+    type: req.body.type,
+    stats: req.body.stats,
+ 
+  };
 
 
-const port = 4000;
+  pokemonData.push(newPokemon);
+
+
+  res.redirect('/pokemon');
+});
+
 app.listen(port, () => {
-  console.log(`Server running on localhost:${port}`);
+  console.log(`Server running on http://localhost:${port}`);
 });
